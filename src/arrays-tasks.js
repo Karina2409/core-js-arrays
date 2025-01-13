e667251e55e27f232a9e8e620b7c4c9de5466074
Karina2409 <karina.siardziuk@mail.ru>
2025-01-13 23:40:21 +0300

    feat: solve the task get-identity-matrix

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -417,8 +417,10 @@ function getFalsyValuesCount(arr) {
  *                              [0,0,0,1,0],
  *                              [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, rowI) =>
+    Array.from({ length: n }, (__, colI) => (rowI === colI ? 1 : 0))
+  );
 }
 
 /**
