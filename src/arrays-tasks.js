fce78d30a46138cad14c02db697470bfdfb784c3
Karina2409 <karina.siardziuk@mail.ru>
2025-01-14 00:08:13 +0300

    feat: solve the task shift-array

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -540,8 +540,9 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
+function shiftArray(arr, n) {
+  const step = ((n % arr.length) + arr.length) % arr.length;
+  return [...arr.slice(-step), ...arr.slice(0, -step)];
 }
 
 /**
